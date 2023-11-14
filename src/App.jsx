2b24ff63dd601d7b83d8d04f03623f18001522e8
Joao Filho <joaofilhomil@gmail.com>
2023-11-14 11:54:47 -0300

    commit test

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
     <h1>Olá Pessoal!!!</h1>
-    <p>Me chamo João Filho</p>
+    <p>Me chamo João </p>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
